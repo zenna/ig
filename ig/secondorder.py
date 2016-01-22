faee724103416f5f2849a559d76ebac4133276ff
Zenna Tavares <zennatavares@gmail.com>
2016-01-22 12:55:00 +0000

    factored out learning

--- ig/secondorder.py
+++ ig/secondorder.py
@@ -116,5 +116,6 @@ def train(network, nprims = 200, nbatch = 50, num_epochs = 500):
 
     return lasagne.layers.get_all_param_values(network)
 
-render, netcost, network = second_order(nprims = nprims, nbatch = 50)
+nprims = 200, nbatch = 50
+render, netcost, network = second_order(nprims = nprims, nbatch = nbatch)
 train(network, nprims = nprims, nbatch = nbatch)
