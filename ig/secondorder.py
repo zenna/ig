0660023d767ed29fd52f06b913a743581277e35e
Zenna Tavares <zennatavares@gmail.com>
2016-01-22 13:41:00 +0000

    removed pooling

--- ig/secondorder.py
+++ ig/secondorder.py
@@ -64,7 +64,8 @@ def second_order(nprims = 200, nbatch = 50):
     net['pool5'] = PoolLayer(net['conv5'], pool_size=3, stride=3, ignore_border=False)
     net['fc6'] = DenseLayer(net['pool5'], num_units=4096)
     net['drop6'] = DropoutLayer(net['fc6'], p=0.5)
-    net['fc7'] = DenseLayer(net['drop6'], num_units=nshape_params, nonlinearity=lasagne.nonlinearities.sigmoid)
+    # net['fc7'] = DenseLayer(net['drop6'], num_units=nshape_params, nonlinearity=lasagne.nonlinearities.sigmoid)
+    net['fc7'] = DenseLayer(net['pool5'], num_units=nshape_params, nonlinearity=lasagne.nonlinearities.sigmoid)
     output_layer = net['fc7']
     output = lasagne.layers.get_output(output_layer)
     scaled_output = output * 2 - 2
@@ -102,12 +103,12 @@ def second_order(nprims = 200, nbatch = 50):
     exfragcoords = gen_fragcoords(width, height)
     print("Compiling Renderer")
     render = make_render(nprims, width, height)
-    return render, netcost, output
+    return render, netcost, output_layer
 
 def train(network, nprims = 200, nbatch = 50, num_epochs = 500):
     width = 224
     height = 224
-    exfragcoords = gen_fragcoords(width, height)
+        exfragcoords = gen_fragcoords(width, height)
     print("Starting Training")
     for epoch in range(num_epochs):
         rand_data = genshapebatch(nprims, nbatch)
