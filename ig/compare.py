86f4ab0487ee3d1b934a99e80b43c49c671838bc
Zenna Tavares <zennatavares@gmail.com>
2016-01-27 14:52:08 +0000

    diff learn alg

--- ig/compare.py
+++ ig/compare.py
@@ -142,12 +142,13 @@ def learn_to_move(nprims = 200, nbatch = 50, width = 224, height = 224):
     # loss = loss1 + loss2
 
     param_diff = T.sum(first_half_delta**2)/nbatch
-    loss2 = -gauss(param_diff, mu=10.0, sigma=100.0)*60000
+    loss2 = -gauss(param_diff, mu=10.0, sigma=100.0)*600
     loss = loss1 + loss2
 
     params = lasagne.layers.get_all_params(output_layer, trainable=True)
-    network_updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.5)
+    # network_updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.5)
     # network_updates = lasagne.updates.adamax(loss, params)
+    network_updates = lasagne.updates.rmsprop(loss, params)
 
     ## Merge Updates
     for k in network_updates.keys():
