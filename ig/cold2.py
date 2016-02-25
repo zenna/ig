08855edadfe11ed88b96d6b02ddb732745f1825b
Zenna Tavares <zennatavares@gmail.com>
2016-02-24 19:45:00 -0500

    training simple way

--- ig/cold2.py
+++ ig/cold2.py
@@ -8,6 +8,27 @@ except:
     print "couldnt import"
 from mayavi import mlab
 
+
+## Extract features from an image
+import lasagne
+import theano
+import theano.sandbox.cuda.dnn
+from lasagne.layers import InputLayer, DenseLayer, DropoutLayer
+if theano.sandbox.cuda.dnn.dnn_available():
+    from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+else:
+    from lasagne.layers import Conv2DLayer as ConvLayer
+
+from lasagne.layers import MaxPool2DLayer as PoolLayer
+from lasagne.layers import LocalResponseNormalization2DLayer as NormLayer
+from lasagne.utils import floatX
+from theano import tensor as T
+from theano import function, config, shared
+import pickle
+
+config.exception_verbosity='high'
+config.optimizer = 'None'
+
 def rand_rotation_matrix(deflection=1.0, randnums=None):
     """
     Creates a random rotation matrix.
@@ -61,24 +82,14 @@ def gen_fragcoords(width, height):
             raster_space[i,j] = np.array([i,j], dtype=config.floatX) + 0.5
     return raster_space
 
-def normalize(v):
-    return v / np.linalg.norm(v)
-
-def set_camera(ro, ta, cr):
-    cw = normalize(ta - ro)
-    cp = np.array([np.sin(cr), np.cos(cr),0.0], dtype=config.floatX)
-    cu = normalize(np.cross(cw,cp))
-    cv = normalize(np.cross(cu,cw))
-    return (cu, cv, cw)
-
 # Append an image filled with scalars to the back of an image.
 def stack(intensor, width, height, scalar):
     scalars = np.ones([width, height, 1], dtype=config.floatX) * scalar
     return np.concatenate([intensor, scalars], axis=2)
 
-def symbolic_render(r, shape_params, raster_space, width, height):
+def symbolic_render(r, raster_space, width, height):
     """Symbolically render rays starting with raster_space according to geometry
-        defined by shape_params"""
+      e  defined by """
     resolution = np.array([width, height], dtype=config.floatX)
     # Normalise it to be bound between 0 1
     norm_raster_space = raster_space / resolution
@@ -95,52 +106,48 @@ def symbolic_render(r, shape_params, raster_space, width, height):
     ro = np.array([0,0,1.5])
 
     # Rotate both by same rotation matrix
-    ro_t = np.dot(ro, r)
-    ndc_t = np.dot(ndc_xyz, r)
+    ro_t = T.dot(ro, r)
+    ndc_t = T.dot(ndc_xyz, r)
 
     # Increment by 0.5 since voxels are in [0, 1]
     ro_t = ro_t + 0.5
     ndc_t = ndc_t + 0.5
     # Find normalise ray dirs from origin to image plane
     unnorm_rd = ndc_t - ro_t
-    rd = unnorm_rd / np.reshape(np.linalg.norm(unnorm_rd, 2, axis=2), (width, height, 1))
+    rd = unnorm_rd / T.reshape(unnorm_rd.norm(2, axis=2), (width, height, 1))
     return rd, ro_t
-
-def make_render(shape_params, width, height):
-    raster_space = gen_fragcoords(width, height)
-    return symbolic_render(shape_params, raster_space, width, height)
 
 def switch(cond, a, b):
     return cond*a + (1-cond)*b
 
-def main(shape_params, rotation_matrix, width, height, nsteps, res):
+def gen_img(shape_params, rotation_matrix, width, height, nsteps, res):
     raster_space = gen_fragcoords(width, height)
-    rd, ro = symbolic_render(rotation_matrix, shape_params, raster_space, width, height)
+    rd, ro = symbolic_render(rotation_matrix, raster_space, width, height)
     a = 0 - ro # c = 0
     b = 1 - ro # c = 1
     tn = a/rd
     tf = b/rd
-    tn_true = np.minimum(tn,tf)
-    tf_true = np.maximum(tn,tf)
+    tn_true = T.minimum(tn,tf)
+    tf_true = T.maximum(tn,tf)
     # do X
     tn_x = tn_true[:,:,0]
     tf_x = tf_true[:,:,0]
-    tmin = np.full(tn_x.shape, 0)
-    tmax = np.full(tn_x.shape, 10)
+    tmin = 0.0
+    tmax = 10.0
     t0 = tmin
     t1 = tmax
-    t02 = switch(tn_x > t0, tn_x, t0)
-    t12 = switch(tf_x < t1, tf_x, t1)
+    t02 = T.switch(tn_x > t0, tn_x, t0)
+    t12 = T.switch(tf_x < t1, tf_x, t1)
     # y
     tn_x = tn_true[:,:,1]
     tf_x = tf_true[:,:,1]
-    t03 = switch(tn_x > t02, tn_x, t02)
-    t13 = switch(tf_x < t12, tf_x, t12)
+    t03 = T.switch(tn_x > t02, tn_x, t02)
+    t13 = T.switch(tf_x < t12, tf_x, t12)
     #z
     tn_x = tn_true[:,:,2]
     tf_x = tf_true[:,:,2]
-    t04 = switch(tn_x > t03, tn_x, t03)
-    t14 = switch(tf_x < t13, tf_x, t13)
+    t04 = T.switch(tn_x > t03, tn_x, t03)
+    t14 = T.switch(tf_x < t13, tf_x, t13)
 
     # Shit a little bit to avoid numerial inaccuracies
     t04 = t04*1.001
@@ -149,37 +156,27 @@ def main(shape_params, rotation_matrix, width, height, nsteps, res):
     # img = np.zeros(width * height)
     left_over = np.ones(width * height)
     step_size = (t14 - t04)/nsteps
-    orig = ro + rd* np.reshape(t04,(width, height, 1))
-    step_size = np.reshape(step_size, (width, height, 1))
+    orig = ro + rd* T.reshape(t04,(width, height, 1))
+    step_size = T.reshape(step_size, (width, height, 1))
     step_size_flat = step_size.flatten()
     xres = yres = zres = res
     nrays = width * height
     nvoxels = xres * yres * zres
-    A = csr_matrix((nrays, nvoxels))
+    # A = csr_matrix((nrays, nvoxels))
     for i in range(nsteps):
         # print "step", i
         pos = orig + rd*step_size*i
-        voxel_indices = np.floor(pos*res)
-        pruned = np.clip(voxel_indices,0,res-1)
-        p_int = pruned.astype('int')
-        indices = np.reshape(p_int, (width*height,3))
-        unique_indices = indices[:,0]*yres*zres + indices[:,1]*zres + indices[:,2]
-        # print unique_indices[50]
-        A_inc = csr_matrix((-step_size_flat, (np.arange(nrays), unique_indices)), shape=(nrays, nvoxels))
-        A = A + A_inc
+        voxel_indices = T.floor(pos*res)
+        pruned = T.clip(voxel_indices,0,res-1)
+        p_int =  T.cast(pruned, 'int32')
+        indices = T.reshape(p_int, (width*height,3))
         attenuation = shape_params[indices[:,0],indices[:,1],indices[:,2]]
-        left_over = left_over*np.exp(-attenuation*np.reshape(step_size, (width * height)))
-        # print "value", np.sum(value)
-        # print "indices", np.sum(indices)
-        # print "pos", np.sum(pos)
-        # img = img + value * left_over
-        # left_over = (1-value)*left_over
+        left_over = left_over*T.exp(-attenuation*step_size_flat)
+
     img = left_over
-    pixels = np.reshape(img,(width, height))
-    # return t14>t04
+    pixels = T.reshape(img,(width, height))
     mask = t14>t04
-    valid_ray_indices = np.nonzero(np.reshape(mask, width*height))[0]
-    return switch(t14>t04, pixels, np.ones(pixels.shape)), A[valid_ray_indices, :], t14>t04, np.reshape(pixels, width*height)[valid_ray_indices]
+    return T.switch(t14>t04, pixels, T.ones_like(pixels))
 
 from matplotlib import pylab as plt
 
@@ -197,45 +194,34 @@ plt.ion()
 width = 100
 height = 100
 res = 256
+nsteps = 100
 ## Example Data
 # x, y, z = np.ogrid[-10:10:complex(res), -10:10:complex(res), -10:10:complex(res)]
 # shape_params = np.sin(x*y*z)/(x*y*z)
 # shape_params = np.clip(shape_params,0,1)
 # shape_params = shape_params - np.min(shape_params) * (np.max(shape_params) - np.min(shape_params))
-shape_params = load_voxels_binary("person_0089.raw", res, res, res)*10.0
+
+
+rotation_matrix = T.matrix()
+shape_params = T.tensor3()
+out = gen_img(shape_params, rotation_matrix, width, height, nsteps, res)
+f = function([shape_params, rotation_matrix], out)
+
+voxel_data = load_voxels_binary("person_0089.raw", res, res, res)*10.0
+f(voxel_data)
+
+# 1. TODO, update it to take in rotation tensor
 #
-# shape_params = np.zeros((res,res,res))
-# q = np.transpose(np.mgrid[0:1:complex(res),0:1:complex(res),0:1:complex(res)],(1,2,3,0))
-# distances = np.sum((q-[0.5,0.5,0.5])**2,axis=3)
-# # shape_params = 1 - distances
-# shape_params =  (distances < 0.3) * 10.0
+one image in creates one voxel grid.  then we render that in 5 different waysself.
 
-nsteps = 500
-rs = []
-As = []
-imgs = []
-for i in range(50):
-    r = rand_rotation_matrix()
-    rs.append(r)
-    print "Drawing", i
-    img, A, mask, img_lim = main(shape_params, r, width, height, nsteps, res)
-    As.append(A)
-    imgs.append(img_lim)
-    plt.figure()
-    plt.imshow(img)
-    plt.draw()
-
-
-from scipy import sparse
-As_t = sparse.vstack(As)
-imgs_t = np.concatenate(imgs)
-log_imgs = np.log(imgs_t)
-
-sol = sparse.linalg.lsqr(As_t, log_imgs)
-voxels = np.reshape(sol[0], (res, res, res))
-img_hyp, A, mask, img_hyp_lim = main(voxels, rand_rotation_matrix(), width, height, nsteps, res)
-plt.figure()
-plt.imshow(img_hyp)
-plt.draw()
-
-mlab.pipeline.volume(mlab.pipeline.scalar_field(voxels/np.max(voxels*10)))
+#
+# def train():
+#     views_per_img = 3
+#     nobjects = 3
+#     # Generate n images with n known different views of an m objects
+#     nsteps = 100
+#     rs = []
+#     As = []
+#     imgs = []
+#     r = rand_rotation_matrix()
+#     img, A, mask, img_lim = main(shape_params, r, width, height, nsteps, res)
