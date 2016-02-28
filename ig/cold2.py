bddf65d969112fbc07652921b58293c961ccbed7
Zenna Tavares <zennatavares@gmail.com>
2016-02-28 17:43:39 -0500

    disable opt

--- ig/cold2.py
+++ ig/cold2.py
@@ -272,7 +272,7 @@ def get_rnd_voxels(n):
     files = filter(lambda x:x.endswith(".raw"), get_filepaths(os.getenv('HOME') + '/data/ModelNet40/chair/train'))
     return np.random.choice(files, n, replace=False)
 
-def train(cost_f, nviews = 3, nvoxgrids=4, res = 128):
+def train(cost_f, render, nviews = 3, nvoxgrids=4, res = 128):
     print "Training"
     npochs = 1000
     for i in range(npochs):
@@ -310,6 +310,6 @@ def main():
     cost, updates = second_order(rotation_matrices, views, width = width, height = height, nsteps = nsteps, res = res, nvoxgrids = nvoxgrids)
     print "Compiling ConvNet"
     cost_f = function([views, rotation_matrices], cost, updates = updates)
-    train(cost_f, nviews = nviews, nvoxgrids = nvoxgrids, res = res)
+    train(cost_f, render, nviews = nviews, nvoxgrids = nvoxgrids, res = res)
 
 main()
